e1f2cd580f6e89b3fe68b2a636f8e5a40d25f5c0
Danila <danilaphiliptsow@gmail.com>
2024-12-06 08:05:02 +0000

    change file paths from names only to full paths

--- Ex_Files/03_04/main.py
+++ Ex_Files/03_04/main.py
@@ -16,7 +16,7 @@ back_to_dict = json.loads(einstein_json)
 print(einstein_json)
 pprint(back_to_dict)
 
-with open("laureates.csv", "r") as f:
+with open("/workspaces/hands-on-python-3084712/Ex_Files/03_04/laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
 
@@ -30,5 +30,5 @@ laureates_beginning_with_a = []
 # LinkedIn learner code here
 
 
-with open("laureates.json", "w") as f:
+with open("/workspaces/hands-on-python-3084712/Ex_Files/03_04/laureates.json", "w") as f:
     json.dump(laureates_beginning_with_a, f, indent=2)
